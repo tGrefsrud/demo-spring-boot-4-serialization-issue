5f7ac3d0ee128ca11f78f23577708539dfe52953
tgrefsrud <thomas.grefsrud@gmail.com>
2025-10-24 18:32:57 +0200

    comment out HibernateConfiguration.java to highlight issue

--- src/main/kotlin/com/example/demo/HibernateConfiguration.java
+++ src/main/kotlin/com/example/demo/HibernateConfiguration.java
@@ -1,18 +1,18 @@
 package com.example.demo;
 
-import org.hibernate.cfg.AvailableSettings;
-
-import org.springframework.boot.hibernate.autoconfigure.HibernatePropertiesCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HibernateConfiguration {
-
-	@Bean
-	HibernatePropertiesCustomizer hibernatePropertiesCustomizer() {
-		return hibernateProperties -> {
-			hibernateProperties.put(AvailableSettings.JSON_FORMAT_MAPPER, JacksonJsonFormatMapper.class.getName());
-		};
-	}
-}
+//import org.hibernate.cfg.AvailableSettings;
+//
+//import org.springframework.boot.hibernate.autoconfigure.HibernatePropertiesCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class HibernateConfiguration {
+//
+//	@Bean
+//	HibernatePropertiesCustomizer hibernatePropertiesCustomizer() {
+//		return hibernateProperties -> {
+//			hibernateProperties.put(AvailableSettings.JSON_FORMAT_MAPPER, JacksonJsonFormatMapper.class.getName());
+//		};
+//	}
+//}
